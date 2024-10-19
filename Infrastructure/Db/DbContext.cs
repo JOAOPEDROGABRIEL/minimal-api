b2ca2ac4f2726289a9cbfa6b5d5e072c8d917174
João Pedro Gabriel <59509614+JOAOPEDROGABRIEL@users.noreply.github.com>
2024-10-19 19:37:29 -0300

    migrations_configured

--- Infrastructure/Db/DbContext.cs
+++ Infrastructure/Db/DbContext.cs
@@ -1,8 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Domain.Entities;
 
 namespace minimal_api.Infrastructure.Db
 {
-    public class DbContext
+    public class DbContexto : DbContext
     {
+        private readonly IConfiguration _configurationAppSettings;
         
+        public DbContexto(IConfiguration configurationAppSettings)
+        {
+            _configurationAppSettings = configurationAppSettings;
+        }
+
+        public DbSet<Administrador> Administradores { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var StringConnection = _configurationAppSettings.GetConnectionString("mysql")?.ToString();
+                if (!string.IsNullOrWhiteSpace(StringConnection))
+                {
+                    optionsBuilder.UseMySql(
+                        StringConnection, 
+                        ServerVersion.AutoDetect(StringConnection)
+                    );
+                }
+            } 
+        }
     }
 }
